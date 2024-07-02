26f880089d9caec6857f118e5481f3ad2b74426b
Muhammad Sendy Adriansyah Prathama <sendy.adriansyah17@gmail.com>
2024-07-02 23:01:10 +0700

    fixed image bug

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import '../../index.css';
 import Navbar from "../Navbar/Navbar.jsx";
+import HomeImg from '../../assets/bg2.jpg'
 
 function Home(){
     return(
         <div className="home" id="Home">
             <Navbar />
 
-            <header className="hero flex justify-center items-center h-screen bg-cover bg-style" style={{backgroundImage: `url('/src/assets/bg2.jpg')`}}>
+            <header className="hero flex justify-center items-center h-screen bg-cover bg-style" style={{backgroundImage: `url(${HomeImg}`}}>
                 <div className="text-center">
                     <h1 className="font-shippori text-white text-7xl drop-shadow-lg mb-3">Welcome to My Portfolio</h1>
                 </div>
